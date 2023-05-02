c29e16761c0102b37ac628b11cdf44018611b309
Connor Broadwell <broadwellconnor@gmail.com>
2023-05-02 10:44:46 -0500

    complete

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,22 @@
-const bottomNavBar = document.querySelector('bottom-nav-bar');
+const navButtons = document.querySelectorAll('.bottom-nav-button');
 
-bottomNavBar
+const handleNavButton = (e) => {
+  if (e.currentTarget.classList.contains('active')) return;
+
+  navButtons.forEach((navButton) => {
+    if (navButton.classList.contains('active')) {
+      navButton.classList.remove('active');
+    }
+  });
+
+  e.currentTarget.classList.add('active');
+}
+
+
+navButtons.forEach((navButton) => {
+  navButton.addEventListener('click', handleNavButton);
+  if (navButton.innerHTML.includes('Home')) {
+    navButton.classList.add('active');
+  }
+});
+
